d10e27cf8f14de84659df2c3e0a703ccab9c2806
Yuval <yuval.shvartsman@gmail.com>
2024-05-10 19:21:40 +0300

    finished setup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import cors from "cors"
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions));
+
+app.get('/getSomething', (req, res) => {
+  console.log('am i even getting here?')
+  res.status(200).json(100)
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`); 
 });
