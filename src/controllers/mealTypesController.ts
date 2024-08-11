b6eb7507208631358aef2baadb71e37ee89463e1
Yuval <yuval.shvartsman@gmail.com>
2024-08-11 21:51:37 +0300

    merge finetuning and small refactor

--- src/controllers/mealTypesController.ts
+++ src/controllers/mealTypesController.ts
@@ -4,14 +4,9 @@ import MealTypes from "../models/MealTypes";
 export const getMealTypes = async (req: Request, res: Response) => {
   try {
     const mealTypes = await MealTypes.find();
-    console.log("🚀 ~ getMealTypes ~ mealTypes:", mealTypes);
     res.json({ data: mealTypes });
   } catch (error) {
     console.log(error);
-    res
-      .status(401)
-      .send(
-        "A problem occured while trying to get meal types from the server."
-      );
+    res.status(401).send("Server Error: Could not get mealTypes.");
   }
 };
--- src/controllers/mealsController.ts
+++ src/controllers/mealsController.ts
@@ -1,87 +1,77 @@
 import { Request, Response } from "express";
 
 import { IMeal, Meals } from "../models/Meals";
-
 import { Ingredient, IIngredient } from "../models/Ingredients";
+
 import { ObjectId } from "mongodb";
 
-type Macros = { amount: number; name: string; _id: string };
+type Macros = { amount: number; name: string; _id: ObjectId };
 
 const getMealMacros = (
   ingredient: IIngredient | null,
   amountInGrams: number,
   mealMacros: Macros[]
-) => {
-  const newMacros: Macros[] | undefined = ingredient?.nutrientValues?.map(
-    (nutrient) => {
-      {
-        return {
-          amount: (nutrient.amount * amountInGrams) / 100, // 6 grams of protein in 100 grams mean 6 grams not 600 :D
-          name: nutrient.nutrientName.name,
-          _id: nutrient.nutrientName._id as string,
-        };
-      }
-    }
-  );
+): Macros[] => {
+  if (!ingredient || !ingredient.nutrientValues) return mealMacros;
 
-  if (newMacros)
-    for (let i = 0; i < newMacros.length; i++) {
-      const existIndex = mealMacros.findIndex(
-        (macro) =>
-          new ObjectId(macro._id).toString() ===
-          new ObjectId(newMacros[i]._id).toString()
-      );
-      const newAmount = newMacros[i].amount;
-      existIndex !== -1
-        ? (mealMacros[existIndex].amount =
-            mealMacros[existIndex].amount + newAmount)
-        : mealMacros.push({ ...newMacros[i] });
-    }
+  const newMacros = ingredient.nutrientValues.map((nutrient) => ({
+    amount: (nutrient.amount * amountInGrams) / 100,
+    name: nutrient.nutrientName.name,
+    _id: nutrient.nutrientName._id as ObjectId,
+  }));
 
-  return mealMacros;
+  return newMacros.reduce((arr, macro) => {
+    const existIndex = arr.findIndex((m) => m._id.equals(macro._id));
+    if (existIndex !== -1) {
+      arr[existIndex].amount += macro.amount;
+    } else {
+      arr.push(macro);
+    }
+    return arr;
+  }, mealMacros);
 };
 
 export const saveMeals = async (req: Request, res: Response) => {
   const { userId, meals } = req.body;
-  console.log("🚀 ~ saveMeals ~ meals:", meals);
-  let createdMealMacros: Macros[] = [];
+
   try {
-    let createdMeals = [];
-    for (let i = 0; i < meals.length; i++) {
-      const mealToCreate: IMeal = meals[i];
+    const createdMeals = await Promise.all(
+      meals.map(async (mealToCreate: IMeal) => {
+        let createdMealMacros: Macros[] = [];
 
-      for (let i = 0; i < mealToCreate.ingredients.length; i++) {
-        const ingredientId = mealToCreate.ingredients[i].ingredient;
+        await Promise.all(
+          mealToCreate.ingredients.map(async (ingredientData) => {
+            const ingredient = await Ingredient.findById(
+              ingredientData.ingredient
+            )
+              .populate({
+                path: "nutrientValues",
+                populate: {
+                  path: "nutrientName",
+                  model: "nutrientsNames",
+                },
+              })
+              .lean({ vitruals: true });
 
-        const ingredient = await Ingredient.findById(ingredientId)
-          .populate({
-            path: "nutrientValues",
-            populate: {
-              path: "nutrientName",
-              model: "nutrientsNames",
-            },
+            createdMealMacros = getMealMacros(
+              ingredient,
+              ingredientData.amount,
+              createdMealMacros
+            );
           })
-          .lean({ vitruals: true });
+        );
 
-        createdMealMacros = [
-          ...getMealMacros(
-            ingredient,
-            mealToCreate.ingredients[i].amount,
-            createdMealMacros
-          ),
-        ];
-      }
-      const createdMeal = await Meals.create({
-        ...{ userId },
-        ...mealToCreate,
-        macros: createdMealMacros,
-      });
+        return Meals.create({
+          ...mealToCreate,
+          userId,
+          macros: createdMealMacros,
+        });
+      })
+    );
 
-      createdMeals.push(createdMeal);
-    }
     res.json({ data: { createdMeals } });
   } catch (error) {
-    console.log(error);
-    res.status(401).send("Invalid token");
+    console.error(error);
+    res.status(500).send("Server Error: Could not save your meal.");
   }
 };
